ac7a44b6b6a5ed0607e19ccfd703779aa203b7fe
deAdi <adi.eberhard@gmail.com>
2026-01-06 18:42:15 +0100

    Refactor log directories and prefixes in config
    Updated log directories and prefixes in the configuration file.

--- config.core.ps1
+++ config.core.ps1
@@ -1,5 +1,5 @@
 # -----------------------------------------------------
-# Konstante Basiskonfiguration – config.core.ps1
+# Konstante Basiskonfiguration â€“ config.core.ps1
 # -----------------------------------------------------
 
 # [DEBUG]
@@ -14,18 +14,8 @@ $logDir5 = "C:\Logs\Macos.Services.Deamon"
 $logDir6 = "C:\Macos\DMSQueue\Error"
 $logDir7 = "C:\Macos\Macos.Shared.Service.Dms"             # neuer Service seit 09.09.25
 $logDir8 = "C:\Macos\Logs\WebApiProxy"                     # neuer WebApiProxy seit Oktober 2025      
-#
 
-<# Log-Verzeichnisse (_logtest aktiv)
-$logDir1 = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\s028042\DMSQueue"
-$logDir2 = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\s028042\Macos.Services.Dms"
-$logDir3 = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\s028042\Macos.WebPks.UI.WebApi"
-$logDir4 = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\s028042\Logs"
-$logDir5 = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\s028042\Macos.Services.Deamon"
-$logDir6 = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\s028042\Error"
-#>
-
-# Dateipräfixe
+# DateiprÃ¤fixe
 $logPrefix1 = "Macos.Services.DmsQueue-Log"
 $logPrefix2 = "Macos.Services.Dms-Log"
 $logPrefix3 = "Macos.WebPks.UI.WebApi_"
@@ -35,5 +25,4 @@ $logPrefix7 = "Macos.Shared.Service.Dms-Log"
 $logPrefix8 = "Macos.WinPks.WebApi.Client.log"
 
 # Ausgabeziel
-#$outputPath = "L:\Bereich_Mitarbeitende\Ordner_Mitarbeitende\AE\_logtest\resultsLogSearchTool.txt"
 $outputPath = "C:\Users\macosService\Desktop\LogSearchTool\resultsLogSearchTool.txt"

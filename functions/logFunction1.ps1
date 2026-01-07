2fabff72edbffd986b1940dc0d8d7c1a3a4d2b86
deAdi <adi.eberhard@gmail.com>
2026-01-07 13:25:14 +0100

    Refactor connection strings in logFunction1.ps1
    Updated connection strings

--- functions/logFunction1.ps1
+++ functions/logFunction1.ps1
@@ -18,11 +18,11 @@ function Search-Log1 {
     )
     $excludeTerms = @(
         "DbName: PKSPAT",
-        "connectionString: Server=S028004A;Database=PKSPAT;MultipleActiveResultSets=true;User Id=MACOS",
-        "connectionStringMaster: Server=S028004A;Database=PKSPATMA;MultipleActiveResultSets=true;User Id=MACOS",
+        "connectionString: Server=S028004A;Database=PKSPAT;MultipleActiveResultSets=true;",
+        "connectionStringMaster: Server=S028004A;Database=PKSPATMA;MultipleActiveResultSets=true;",
         "DbName: TKSPAT",
-        "connectionString: Server=S028004A;Database=TKSPAT;MultipleActiveResultSets=true;User Id=MACOS",
-        "connectionStringMaster: Server=S028004A;Database=TKSPATMA;MultipleActiveResultSets=true;User Id=MACOS",
+        "connectionString: Server=S028004A;Database=TKSPAT;MultipleActiveResultSets=true;",
+        "connectionStringMaster: Server=S028004A;Database=TKSPATMA;MultipleActiveResultSets=true;",
         "HttpMessageHandler expired after",
         "Starting HttpMessageHandler cleanup cycle with",
         "Ending HttpMessageHandler cleanup cycle after",
